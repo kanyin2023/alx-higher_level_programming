31f45a196005da3cfecf0ceae814201372a412db
kanyin2023 <folorunsomaryam@gmail.com>
2023-11-28 20:58:20 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
+#!/usr/bin/python3
+# function that prints a string in uppercase followed by a new line
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    letter = ""
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            letter += chr(ord(i) - 32)
+        else:
+            letter += i
+    print("{}".format(letter))
