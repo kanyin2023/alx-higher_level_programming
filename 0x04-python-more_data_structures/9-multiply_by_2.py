acd074f045411e3d7294fede67b1d11edf8958ce
kanyin2023 <folorunsomaryam@gmail.com>
2023-12-06 12:36:50 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict and len(my_dict):
-        max = list(my_dict.keys())[0]
-        for key in my_dict:
-            if my_dict[key] > my_dict[max]:
-                max = key
-        return max
-    return None
+def multiply_by_2(my_dict):
+    return {key: value * 2 for key, value in my_dict.items()}
