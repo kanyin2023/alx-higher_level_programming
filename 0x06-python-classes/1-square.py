2ee681663df43e6f554399bf36f467e867911963
kanyin2023 <folorunsomaryam@gmail.com>
2023-12-20 18:59:59 -0800

    103-magic_class.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a class square"""
+
+
+class Square:
+    """reprensts a class"""
+
+    def __init__(self, x):
+        """initializing private instance attribute 'size'
+
+        Args:
+            x (int): size of the square
+        """
+        self.__size = x
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """represents a class Square"""
+
+    def __init__(self, size=0):
+        """initializing a private attribute 'size'
+
+        Args:
+            size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+
+        else:
+            self.__size = size
+
+        if size < 0:
