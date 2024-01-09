4b27d3808b874c757308ae0e0315ef81ee6824b7
kanyin2023 <folorunsomaryam@gmail.com>
2024-01-09 05:40:54 -0800

    101-stats.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""
-function that appends a string
-"""
+"""Defines a file-writing function."""
 
 
-def append_write(filename="", text=""):
-    """eturns the number of characters added:"""
-    with open(filename, 'a', encoding='utf=8') as f:
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
