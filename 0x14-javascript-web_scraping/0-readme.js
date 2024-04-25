e9ac31960adc9625a8c084d2ea65ec4470359bd1
kanyin2023 <folorunsomaryam@gmail.com>
2024-04-24 21:17:54 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
+// script that reads and prints the content of a file.
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // log the content of the file
+    console.log(data);
+  }
+})
